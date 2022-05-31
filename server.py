6b0a2ff093d718def5520d817de19ae60420ce7c
Henri <henridanielpenadequero@gmail.com>
2022-05-31 10:15:23 -0400

    request to get in and broadcast done

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 
 import hashlib
+import time
+from requests import request
 import zmq
 import netifaces as ni
 import pickle
 import json
+import threading
 
 PORT = '8002'
 
@@ -11,7 +14,7 @@ class Node():
     def __init__(self):
         self.ip, self.broadcast = self.get_ip_broadcast()
         self.id = self.get_id(self.ip)
-        self.chor = [] # {hash: obj_addr}
+        self.chord = [] # {hash: obj_addr}
         self.successor_id = None
         self.successor_ip = None
         self.antecessor_id = None
@@ -24,12 +27,21 @@ class Node():
         ##----EL ADDRESS ES DEL IP DE BROADCAST O DEL IP DEL NODO?--------##
         address = "tcp://"+ self.broadcast +":"+ PORT
         self.socket_sub.bind(address) 
-        ##----COMO PUEDO TOMAR LOS DATOS piden unirse o la tabla chor?----##
+        broadcast_lock = threading.Lock()
+        #Open Thread
+        broadcast_thread = threading.Thread(target = broadcast_thread_funct, args = (self,broadcast_lock,self.socket_pub))
+        broadcast_thread.start()
+        broadcast_thread.join()
+        ##----COMO PUEDO TOMAR LOS DATOS piden unirse o la tabla chord?----##
         address = "tcp://"+ self.ip +":"+ PORT
         self.socket_pull.bind(address) 
         #------------------------------------------------------------------# 
         self.socket_push = self.context.socket(zmq.PUSH)
         self.socket_pull = self.context.socket(zmq.PULL)
+        self.socket_pull.bind(self.ip)
+        pull_lock = threading.Lock()
+        #open thread
+        pull_thread = threading.Thread( target= pull_thread_funct, args = (self, pull_lock, self.socket_pull))
         self.get_in()
         self.finger_table = [] # [("id1", "ip1"), ("id2", "ip2")]
         self.my_objects = {} # {"id1": "tal lugar", "id2": "mas cual lugar"}
@@ -58,20 +70,74 @@ class Node():
         socket = self.socket_pub
         address = "tcp://"+ self.broadcast +":"+ PORT
         socket.bind(address)  
-        socket.send_string('I-get-in-bitches')
+        socket.send_string('{"I-get-in": "'+ self.ip +'"}')
         # get message from successor
         #-----------------------------------------------------------------#
         ##----------ESTA BIEN TOMAR LOS DATOS DE ESA FORMA?--------------##
         socket = self.socket_pull
         address = "tcp://"+ self.ip +":"+ PORT
-        socket.bind(address)  
-        recv_json =  json.loads(socket.recv_json())
-        self.successor_id = recv_json["successor_id"]
-        self.successor_ip = recv_json["successor_ip"]
-        self.antecessor_id = recv_json["antecessor_id"]
-        self.antecessor_ip = recv_json["antecessor_ip"]
+        socket.bind(address)
+        lock = threading.Lock()
+        #Open Thread
+        t = threading.Thread(target = request, args = (self,lock,socket))
+        t.start()
+        t.join()
+        
         #-----------------------------------------------------------------#
         return
+
+    #subscriter to broadcast
+    def broadcast_thread_funct(self,lock,socket_pub)->None:
+        while True:
+            recv_json =  json.loads(socket_pub.recv_json())
+            if recv_json: #if there is something in recv_json
+                if("I-get-in" in recv_json):
+                    lock.acquire()
+                    ret_json = {"send-me-confirmation":self.ip}
+                    self.socket_push.bind(recv_json["I-get-in"])
+                    self.socket_push.send_json(ret_json)
+                    lock.release()
+        
+    def pull_thread_funct(self,lock,socket_pull)->None:
+        while True:
+            recv_json =  json.loads(socket_pull.recv_json())
+            if recv_json: #if there is something in recv_json
+                if("my-confirmation" in recv_json):
+                    lock.acquire()
+                    ret_json = {}
+                    ret_json["you-are-in"]["successor_id"] = self.id
+                    ret_json["you-are-in"]["successor_ip"] = self.ip
+                    ret_json["you-are-in"]["antecessor_id"] = self.antecessor_id
+                    ret_json["you-are-in"]["antecessor_ip"] = self.antecessor_ip
+                    self.socket_push.bind(recv_json["my-confirmation"])
+                    self.socket_push.send_json(ret_json)
+                    lock.release()
+    # wait request
+    def request (self,lock,socket)-> json:
+        start_time = time.time()
+        recived = False
+        while True:
+            actual_time = time.time() 
+            if(actual_time-start_time > 30):
+                break
+            recv_json =  json.loads(socket.recv_json())
+            if recv_json: #if there is something in recv_json
+                if("send-me-confirmation" and not recived):
+                    lock.acquire()
+                    ret_json = {"my-confirmation":self.ip}
+                    self.socket_push.bind(recv_json["send-me-confirmation"])
+                    self.socket_push.send_json(ret_json)
+                    lock.release()
+                    recived = True
+                if("you-are-in" in recv_json):
+                    lock.acquire() 
+                    self.successor_id = recv_json["successor_id"]
+                    self.successor_ip = recv_json["successor_ip"]
+                    self.antecessor_id = recv_json["antecessor_id"]
+                    self.antecessor_ip = recv_json["antecessor_ip"]
+                    lock.release()
+                    break # break out of loop and end
+        return recv_json
         
     def update_finger_table(self) -> None:
         # update my objects
@@ -86,7 +152,7 @@ class Node():
         address = "tcp://"+ self.ip +":"+ PORT
         socket.bind(address)  
         recv_json =  json.loads(socket.recv_json())
-        self.chor = recv_json
+        self.chord = recv_json
         #-----------------------------------------------------------------#
         # update other objects
         socket = self.socket_push 
